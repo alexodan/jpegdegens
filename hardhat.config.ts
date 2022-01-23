2901570d714c1380a3d7ed4c85b5a76cf08f866e
Alexis Diaz <alexdan1993@gmail.com>
2022-01-23 17:01:17 -0300

    Env and hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,14 @@
-import "@nomiclabs/hardhat-ethers"
-import "@nomiclabs/hardhat-waffle"
+import '@nomiclabs/hardhat-ethers'
+import '@nomiclabs/hardhat-waffle'
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: '0.8.4',
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+  },
 }
